db35e9f1edc32cbdf571f63f5e1eb60cb58f6866
ilias-zet <76498638+ilias-zet@users.noreply.github.com>
2025-01-31 16:57:42 +0100

    big file processing possibility added

--- index.php
+++ index.php
@@ -1,57 +1,45 @@
 <?php
-  $input1 = read_input("input1.txt");
-  $input2 = read_input("input2.txt");
-
-  [$output1, $output2] = find_difference($input1, $input2);
+  $input1 = fopen("input1.txt", 'r');
+  $input2 = fopen("input2.txt", 'r');
+  $output1 = fopen("output1.txt", 'w');
+  $output2 = fopen("output2.txt", 'w');
   
-  write_output("output1.txt", $output1);
-  write_output("output2.txt", $output2);
-
-  function read_input(string $filename) {
-    $data = file_get_contents($filename);
-
-    if ($data) {
-      return explode("\n", $data);
-    } else {
-      return [];
-    };
+  if (!$input1 || !$input2 || !$output1 || !$output2) {
+    die("Error opening files");
   }
 
-  function write_output(string $filename, array $array) {
-    $result = implode("\n", $array);
-    file_put_contents($filename, $result);
+  function read_next_line($input) {
+    return trim(fgets($input), "\n\r");
+  }
+  
+  $line1 = read_next_line($input1);
+  $line2 = read_next_line($input2);
+
+  while ($line1 != false && $line2 != false) {
+    if ($line1 === $line2) {
+      $line1 = read_next_line($input1);
+      $line2 = read_next_line($input2);
+    } elseif ($line1 < $line2) {
+      fwrite($output1, $line1."\n");
+      $line1 = read_next_line($input1);
+    } elseif ($line1 > $line2) {
+      fwrite($output2, $line2."\n");
+      $line2 = read_next_line($input2);
+    }
   }
 
-  function find_difference(array $input1, array $input2) {
-    $only_in_input1 = [];
-    $only_in_input2 = [];
-
-    $i = 0;
-    $z = 0;
-
-    while ($i < count($input1) && $z < count($input2)) {
-      if ($input1[$i] === $input2[$z]) {
-        $i++;
-        $z++;
-      } elseif ($input1[$i] < $input2[$z]) {
-        $only_in_input1[] = $input1[$i];
-        $i++;
-      } elseif ($input1[$i] > $input2[$z]) {
-        $only_in_input2[] = $input2[$z];
-        $z++;
-      }
-    }
-
-    while ($i < count($input1)) {
-      $only_in_input1[] = $input1[$i];
-      $i++;
-    }
-
-    while ($z < count($input2)) {
-      $only_in_input2[] = $input2[$z];
-      $z++;
-    }
-
-    return [$only_in_input1, $only_in_input2];
+  while ($line1 != false) {
+    fwrite($output1, $line1."\n");
+    $line1 = read_next_line($input1);
   }
+
+  while ($line2 != false) {
+    fwrite($output2, $line2."\n");
+    $line2 = read_next_line($input2);
+  }
+
+  fclose($input1);
+  fclose($input2);
+  fclose($output1);
+  fclose($output2);
 ?>
